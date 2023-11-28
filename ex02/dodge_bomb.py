eeaeb0ec15dc71950c676f1daa4e4d406228264e
Daichi <c0a2209729@edu.teu.ac.jp>
2023-11-28 14:09:35 +0900

    爆弾surface追加

--- ex02/dodge_bomb.py
+++ ex02/dodge_bomb.py
@@ -1,5 +1,6 @@
 import sys
 import pygame as pg
+import random
 
 
 WIDTH, HEIGHT = 1600, 900
@@ -11,9 +12,11 @@ def main():
     bg_img = pg.image.load("ex02/fig/pg_bg.jpg")
     kk_img = pg.image.load("ex02/fig/3.png")
     kk_img = pg.transform.rotozoom(kk_img, 0, 2.0)
-    enn = pg.Surface((20, 20))
-    pg.draw.circle(enn, (255, 0, 0), (10, 10), 10)
-    enn.set_colorkey((0, 0, 0))
+    rb_img = pg.Surface((20, 20))
+    pg.draw.circle(rb_img, (255, 0, 0), (10, 10), 10)
+    rb_img.set_colorkey((0, 0, 0))
+    rb_x = random.randint(0,900)
+    rb_y = random.randint(0,400)
     clock = pg.time.Clock()
     tmr = 0
     while True:
@@ -23,7 +26,7 @@ def main():
 
         screen.blit(bg_img, [0, 0])
         screen.blit(kk_img, [900, 400])
-        screen.blit(enn, [100, 400])
+        screen.blit(rb_img, [rb_x, rb_y])
         pg.display.update()
         tmr += 1
         clock.tick(10)
